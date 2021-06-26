7d1fd06b5ace6adef565276f1ff0bc45446a2ddd
Ajay1910-svg <84716743+Ajay1910-svg@users.noreply.github.com>
2021-06-26 11:40:41 +0530

    Create ari.c

--- ari.c
+++ ari.c
@@ -21,31 +21,31 @@ if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
   }
   float grade=(4.71*(float)letterscount/(float)wordcount)+(0.5*(float)wordcount/(float)sentencecount)-21.43;
   if(grade<=1&&grade>0)
-  printf("Kindergarten\n");
+  printf("Kinder garten\n");
   else if(grade<=2&&grade>1)
-  printf("first/secondgrade\n");
+  printf("first/ secondgrade\n");
   else if(grade<=3&&grade>2)
-  printf("thirdgrade\n");
+  printf("third grade\n");
   else if(grade<=4&&grade>3)
-  printf("fourthgrade\n");
+  printf("fourth grade\n");
   else if(grade<=5&&grade>4)
-  printf("fifthgrade\n");
+  printf("fifth grade\n");
   else if(grade<=6&&grade>5)
-  printf("sixthgrade\n");
+  printf("sixth grade\n");
   else if(grade<=7&&grade>6)
-  printf("seventhgrade\n");
+  printf("seventh grade\n");
   else if(grade<=8&&grade>7)
-  printf("eighthgrade\n");
+  printf("eighth grade\n");
   else if(grade<=9&&grade>8)
-  printf("ninthgrade\n");
+  printf("ninth grade\n");
   else if(grade<=10&&grade>9)
-  printf("tenthgrade\n");
+  printf("tenth grade\n");
   else if(grade<=11&&grade>10)
-  printf("eleventhgrade\n");
+  printf("eleventh grade\n");
   else if(grade<=12&&grade>11)
-  printf("twelfthgrade\n");
+  printf("twelfth grade\n");
   else if(grade<=13&&grade>12)
-  printf("collegestudent\n");
+  printf("college student\n");
   else
   printf("Proffessor");
   }
